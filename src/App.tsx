f939d5818a8008bc3ca007dc54c900bca3a11813
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-15 17:10:49 +0900

    Add example component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./components/organisms/Example"
+import Example from "./components/organisms/Example";
 
 const App: React.FC = () => {
   return (
@@ -18,6 +20,7 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
+        <Example/>
       </header>
     </div>
   );
